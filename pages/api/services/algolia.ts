d2849967a81adaf6b99abf798d652c9835b1882c
Jaime Santos Alcon <jaime.santosalcon@contentstack.com>
2023-03-23 13:35:29 -0400

    updates

--- pages/api/services/algolia.ts
+++ pages/api/services/algolia.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import BaseService from "./base";
 import { EntryEmbedable } from "@contentstack/utils/dist/types/Models/embedded-object";
 import { Utils } from "contentstack";
+import fs from "fs";
 
 const renderOption = {
   ["span"]: (node: any, next: any) => {
@@ -21,31 +22,36 @@ const flattenObj = (obj: any): any => {
   return fakeEntry.field.replace(/<[^>]+>/g, "");
 };
 const flattenJson = (obj: any): void => {
-  for (const [k, val] of Object.entries(obj)) {
-    const v = val as any;
-    if (typeof v === "object" && v.hasOwnProperty("type") && v["type"] === "doc") {
-      obj[k] = flattenObj(v);
-    } else if (typeof v === "object") {
-      if (Array.isArray(v)) {
-        const newArray = [];
+  try {
+    for (const [k, val] of Object.entries(obj)) {
+      // console.log(" Parsing >>>", k);
+      const v = val as any;
+      if (typeof v === "object" && v.hasOwnProperty("type") && v["type"] === "doc") {
+        obj[k] = flattenObj(v);
+      } else if (typeof v === "object") {
+        if (Array.isArray(v)) {
+          const newArray = [];
 
-        for (let i = 0; i < v.length; i++) {
-          const subObj = v[i];
-          if (typeof subObj === "object" && subObj.hasOwnProperty("type") && subObj["type"] === "doc") {
-            newArray.push(flattenObj(subObj));
-          } else {
-            flattenJson(v[i]);
-            newArray.push(v[i]);
+          for (let i = 0; i < v.length; i++) {
+            const subObj = v[i];
+            if (typeof subObj === "object" && subObj.hasOwnProperty("type") && subObj["type"] === "doc") {
+              newArray.push(flattenObj(subObj));
+            } else {
+              flattenJson(v[i]);
+              newArray.push(v[i]);
+            }
           }
+          // console.log(">>> New Array", newArray);
+          obj[k] = newArray;
+        } else {
+          flattenJson(v);
         }
-        // console.log(">>> New Array", newArray);
-        obj[k] = newArray;
-      } else {
+
         flattenJson(v);
       }
-
-      flattenJson(v);
     }
+  } catch (e) {
+    obj = { error: e };
   }
 };
 
@@ -61,7 +67,13 @@ export default class AlgoliaService extends BaseService {
   async execute() {
     try {
       const body = this.getRequestBody();
-      flattenJson(body);
+      console.log("body", typeof body);
+      // fs.writeFileSync(
+      //   "/Users/jaimesantosalcon/dev/cs/impl/api-nextjs/body.json",
+      //   JSON.stringify(typeof body === "string" ? JSON.parse(body) : body)
+      // );
+      flattenJson(typeof body === "string" ? JSON.parse(body) : body);
+      // console.log("body", JSON.parse(JSON.stringify(body)));
       this.success(body);
     } catch (e) {
       console.log(e);
