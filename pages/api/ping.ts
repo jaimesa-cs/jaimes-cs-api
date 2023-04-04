6ce75bc411f44205684b11c7f3cf9f9b1e206b35
Jaime Santos Alcon <jaime.santosalcon@contentstack.com>
2023-04-04 16:58:15 -0400

    updated hello service

--- pages/api/ping.ts
+++ pages/api/ping.ts
@@ -6,5 +6,5 @@ type Data = {
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ msg: "Hello!" });
+  res.status(200).json({ msg: "Pong!" });
 }
